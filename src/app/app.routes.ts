546668442d23e5ff88b80e5a020f73debddb4ba4
Manuel Jesús López Martínez <manulopezayora1985@protonmail.com>
2025-07-28 11:24:45 +0200

    chore(routes): clean code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: 'calculator',
+        path: '',
         loadComponent: () => import('./calculator/view/calculator-view/calculator-view')
     },
     {
         path: '**',
-        redirectTo: 'calculator'
+        redirectTo: ''
     }
 ];
